cf74160488ac363fbc09922f3a3d33d0f5e446a5
ram kumar <rr697144@gmail.com>
2024-11-08 12:52:37 +0530

    public modifier

---
+++ oops/accessmodifier.php
@@ -0,0 +1,32 @@
+<?php
+
+echo "<h3>Access modifier in PHP OOPs</h3>";
+
+class base{
+    public $name;
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    public function show(){
+        echo "Your name is $this->name <br>";
+    }
+}
+
+class child extends base{
+
+}
+
+
+
+$base1 = new base("Ram kumar");
+$base1->show();
+
+$child = new child("child class ");
+
+$child->show();
+
+
+
+?>
