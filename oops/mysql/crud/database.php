619b48ef401d20bceb259a290319dee61ffc1a0c
ram kumar <rr697144@gmail.com>
2024-11-12 07:48:15 +0530

    insert data in database using PHP OOPs class

--- oops/mysql/crud/database.php
+++ oops/mysql/crud/database.php
@@ -16,6 +16,7 @@ class Database{
     {
         if(!$this->conn){
             $this->mysqli = new mysqli($this->db_host,$this->db_username,$this->db_password,$this->db_name);
+            $this->conn = true;
         }
         
         if($this->mysqli->connect_error){
@@ -27,8 +28,24 @@ class Database{
     }
 
     // function to insert into database
-    public function insert(){
+    public function insert($table,$params = array()){
+        if($this->tableExists($table)){
 
+            $table_columns = implode(", ",array_keys($params));
+            $table_value = implode("', '",$params);
+            $sql = "INSERT INTO $table ($table_columns) VALUES ('$table_value')";
+
+            if($this->mysqli->query($sql)){
+                array_push($this->result,$this->mysqli->insert_id);
+                return true;
+            }else{
+                array_push($this->result,$this->mysqli->error);
+                return false;
+            }
+
+        }else{
+            return false;
+        }
     }
 
     // function to update row in database
@@ -45,6 +62,28 @@ class Database{
     // function to fetch data from database
     public function select(){
 
+    }
+
+    // check table exist or not
+    private function tableExists($tableName){
+        $sql = "SHOW TABLES FROM $this->db_name LIKE '$tableName'";
+        $tableInDb = $this->mysqli->query($sql);
+
+        if($tableInDb){
+            if($tableInDb->num_rows == 1){
+                return true;
+            }else{
+                array_push($this->result, $tableName. "Does not exist in database");
+                return false;
+            }
+        }
+    }
+
+    // get Result
+    public function getResult(){
+        $val = $this->result;
+        $this->result = array();
+        return $val;
     }
 
     // close connection
--- oops/mysql/crud/index.php
+++ oops/mysql/crud/index.php
@@ -4,4 +4,10 @@ include "database.php";
 
 $obj = new Database();
 
+$obj->insert('student',["name" => "Charuu",'age' => "17", 'city' => "Delhi"]);
+
+
+echo "Insert id ";
+print_r($obj->getResult());
+
 ?>
