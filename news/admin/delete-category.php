c84cca16046ec7abe37f26b87be62c262061c794
ram kumar <rr697144@gmail.com>
2024-11-14 07:47:52 +0530

    delete category code is create

---
+++ news/admin/delete-category.php
@@ -0,0 +1,11 @@
+<?php
+include 'config.php';
+
+$id = $_GET['id'];
+$slq = "DELETE FROM category WHERE category_id = {$id}";
+if (mysqli_query($conn, $slq)) {
+    header("Location: {$hostname1}/admin/category.php");
+} else {
+    echo "<h2 class = 'text-warning'>Unable to delete user</h2>";
+}
+mysqli_close($conn);
